33c1d636fc79bae874ef3caa2d4c0c9d8280c9ee
snella <snella@DJ22D03239>
2023-09-26 10:05:42 +0100

    Add project files.

---
+++ Gambling Simulator 2K23/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim randoms(6) As Integer
+        Dim money As Integer
+        Dim valid = False
+
+    End Sub
+End Module
